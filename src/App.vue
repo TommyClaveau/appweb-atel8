361224d76c8eea0b13901860e48e93c35a4db6c2
TommyClaveau <2351734@csfoy.ca>
2025-02-18 14:50:45 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import TimeLine from "./components/TimeLine.vue";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+</script>
+
+<template>
+  <div class="container">
+    <TimeLine />
+  </div>
+</template>
+
+<style scoped></style>
